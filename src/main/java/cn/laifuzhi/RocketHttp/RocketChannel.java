bc2f0dbe5244d29ce946bcc33928f6fe34d83727
fuzhi.lfz <fuzhi.lfz@alibaba-inc.com>
2020-11-08 03:07:22 +0800

    异步+连接池

--- src/main/java/cn/laifuzhi/RocketHttp/RocketChannel.java
+++ src/main/java/cn/laifuzhi/RocketHttp/RocketChannel.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @AllArgsConstructor
-public class RocketChannel {
+public final class RocketChannel {
     // 刚刚创建出来时，channelFuture中的channel可能还没有连接完成，isActive还是false
     private volatile boolean firstUsed;
     private ChannelFuture channelFuture;
