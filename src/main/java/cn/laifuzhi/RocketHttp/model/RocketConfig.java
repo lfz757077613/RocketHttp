f5a900c9f0b5022144fe4289391919e631526107
fuzhi.lfz <fuzhi.lfz@alibaba-inc.com>
2020-11-12 19:05:10 +0800

    去掉guava依赖，去掉lombok配置，保持最小依赖

--- src/main/java/cn/laifuzhi/RocketHttp/model/RocketConfig.java
+++ src/main/java/cn/laifuzhi/RocketHttp/model/RocketConfig.java
@@ -13,7 +13,7 @@ public final class RocketConfig {
     private int requestTimeout = 60 * 1000;
     private int httpMaxContent = 10 * 1024 * 1024;
     private int maxConnectPerHost = 100;
-    private int evictIdleConnectPeriod = 60 * 1000;
+    private int evictIdleConnectPeriod = 5 * 60 * 1000;
     private int idleConnectKeepAliveTime = 60 * 1000;
     /**
      * 获取不到连接时是否阻塞
